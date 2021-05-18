1ea1b1cc4a384a88b2b9261b1bb6ba5f59a9c403
Pahuka <J-a-c-k@yandex.ru>
2021-05-18 12:33:32 +0500

    Реализовал работу метода GetNextMove в несколько потоков.

--- Bot_Parallel.cs
+++ Bot_Parallel.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace rocket_bot
@@ -9,9 +8,22 @@ namespace rocket_bot
         public Rocket GetNextMove(Rocket rocket)
         {
             // TODO: распараллелить запуск SearchBestMove
+
+            if (this.threadsCount == 2)
+            {
+                var task1 = Task.Factory.StartNew(() => 
+                    SearchBestMove(rocket, new Random(random.Next()), iterationsCount / 2));                
+                var task2 = Task.Factory.StartNew(() => 
+                    SearchBestMove(rocket, new Random(random.Next()), iterationsCount / 2));
+                var one = task1.Result.Item2;
+                var two = task2.Result.Item2;
+                return (task1.Result.Item2 > task2.Result.Item2) ?
+                    rocket.Move(task1.Result.Item1, level) : rocket.Move(task2.Result.Item1, level);
+            }
+
             var bestMove = SearchBestMove(rocket, new Random(random.Next()), iterationsCount);
-            var newRocket = rocket.Move(bestMove.Item1, level);
-            return newRocket;
+
+            return rocket.Move(bestMove.Item1, level);
         }
     }
 }
